88fc103c52cf2a412610e289c88db8d55648f228
derekleung <cwleung@duck.com>
2022-10-13 22:51:49 +0100

    positional encoding

--- embedding/transformer_v2.py
+++ embedding/transformer_v2.py
@@ -94,6 +94,22 @@ class TransformerEncoder(nn.Module):
         return attention_maps
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe, persistent=False)
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.size(1)]
+        return x
+
+
 if __name__ == '__main__':
     bsz = 64
     seqlen = 1024
