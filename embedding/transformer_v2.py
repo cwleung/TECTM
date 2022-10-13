bbcc13af1583d1712b056e3019a6777a463fabc0
derekleung <cwleung@duck.com>
2022-10-13 22:49:13 +0100

    transformer_v2.py

---
+++ embedding/transformer_v2.py
@@ -0,0 +1,111 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from embedding.sliding_chunks import sliding_chunks_matmul_qk, sliding_chunks_matmul_pv
+
+
+def scaled_dot_product(q, k, v, mask=None, w=128):
+    d_k = q.size()[-1]
+    attn_logits = sliding_chunks_matmul_qk(q=q, k=k, w=w, padding_value=0)
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    # TODO add dropout
+    values = sliding_chunks_matmul_pv(prob=attention, v=v, w=w)
+    return values, attention
+
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        self.qkv_proj = nn.Linear(input_dim, 3 * embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, _ = x.size()
+        qkv = self.qkv_proj(x)
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3 * self.head_dim)
+        q, k, v = qkv.chunk(3, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.reshape(batch_size, seq_length, self.embed_dim)
+        o = self.o_proj(values)
+        if return_attention:
+            return o, attention
+        else:
+            return o
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, input_dim, num_heads, dim_feedforward, dropout=0.0):
+        super().__init__()
+        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.linear_net = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim)
+        )
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        attn_out = self.self_attn(x, mask=mask)
+        x = x + self.dropout(attn_out)
+        x = self.norm1(x)
+        linear_out = self.linear_net(x)
+        x = x + self.dropout(linear_out)
+        x = self.norm2(x)
+        return x
+
+
+class TransformerEncoder(nn.Module):
+
+    def __init__(self, num_layers, **block_args):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderBlock(**block_args) for _ in range(num_layers)])
+
+    def forward(self, x, mask=None):
+        for l in self.layers:
+            x = l(x, mask=mask)
+        return x
+
+    def get_attention_maps(self, x, mask=None):
+        attention_maps = []
+        for l in self.layers:
+            _, attn_map = l.self_attn(x, mask=mask, return_attention=True)
+            attention_maps.append(attn_map)
+            x = l(x)
+        return attention_maps
+
+
+if __name__ == '__main__':
+    bsz = 64
+    seqlen = 1024
+    input_dim = 1024
+    embed_dim = 128
+    num_heads = 8
+    head_dim = embed_dim // num_heads
+    transformer = TransformerEncoder(num_layers=2,
+                                     input_dim=input_dim,
+                                     dim_feedforward=128,
+                                     num_heads=8,
+                                     dropout=0.3)
+
+    input_seq = torch.randn((bsz, seqlen, input_dim))
+    transformer(input_seq)
