2d4e7db8b107c0ff1f1bf26d65eb1ef6e70ba565
derekleung <cwleung@duck.com>
2022-11-13 01:35:16 +0000

    Move Transformer tests cases

--- embedding/transformer.py
+++ embedding/transformer.py
@@ -54,32 +54,3 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
-
-
-if __name__ == '__main__':
-    ntoken = 256
-    nlayers = 1
-    ninp = 32
-    nhead = 2
-    nhid = 128
-    dropout = 0
-    encoder_layers = 1
-    encoder_norm = LayerNorm(ninp)
-    encoder = nn.Embedding(ntoken, ninp)
-    transformer_encoder = TransformerEncoder(num_layers=nlayers,
-                                             input_dim=ninp,
-                                             dim_feedforward=nhid,
-                                             num_heads=nhead,
-                                             dropout=dropout)
-
-    seqlen = 32
-    batch_size = 16
-    x = torch.randint(ntoken, size=(batch_size, seqlen))
-    print(x.shape)
-    assert x.shape == torch.Size((batch_size, seqlen))
-    x = encoder(x)
-    print(x.shape)
-    assert x.shape == torch.Size((batch_size, seqlen, ninp))
-    x = transformer_encoder(x)
-    print(x.shape)
-    assert x.shape == torch.Size((batch_size, seqlen, ninp))
